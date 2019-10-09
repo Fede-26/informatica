9912160b752df2bff328ca22e1a6c4d7cc73d34c
Federico Zotti <fedezotti.fz@gmail.com>
2019-10-09 11:18:50 +0000

    add exercise

--- cicli/76-11.py
+++ cicli/76-11.py
@@ -1,1 +1,21 @@
+#DOESN'T WORK
+#!/usr/bin/env python3
+
 num = int(input("num: "))
+
+def is_prime(n):
+    if n > 1:
+        for i in range(2, n//2):
+            if (n % i) == 0:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+
+i = 1
+while (i <= num):
+    if (is_prime(i)):
+        print(i)
+    i += 1
